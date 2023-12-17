233ad69e09aed7fb1319fc174215d7c2e9b0983a
João Luis Borges Lima <joaoluisborgeslima@gmail.com>
2023-12-17 19:36:16 -0300

    link amigoSecret

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ const Page = () => {
   return (
     <div>
       <h1 className="text-center text-4xl my-3">HOME - Amigo Secreto</h1>
+
+      <button><a href="/event/1" >Buscar Amigo Secreto</a></button>
     </div>
   );
 }
