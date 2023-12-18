af54db7547ee8bc36b5d8d8f1f9c16e179578c2a
João Luis Borges Lima <joaoluisborgeslima@gmail.com>
2023-12-18 17:58:48 -0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-center text-4xl my-3">HOME - Amigo Secreto</h1>
-
-      <button className="flex bg-blue-600 py-2 px-3 rounded-lg items-center"><a href="/event/1" >Buscar Amigo Secreto</a></button>
+    <div >
+      <div className="text-center">
+        <h1 className="text-4xl my-7">HOME - Amigo Secreto</h1>
+        <button className=" bg-blue-600 py-2 px-3 rounded-lg "><a href="/event/1" >Buscar Amigo Secreto</a></button>
+      </div>
     </div>
   );
 }
