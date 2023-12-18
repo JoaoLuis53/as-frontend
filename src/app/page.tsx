59c7d89ea17fd7bb0c06be038267e01ca7b13f78
João Luis Borges Lima <joaoluisborgeslima@gmail.com>
2023-12-18 16:45:41 -0300

    estilização do botão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ const Page = () => {
     <div>
       <h1 className="text-center text-4xl my-3">HOME - Amigo Secreto</h1>
 
-      <button><a href="/event/1" >Buscar Amigo Secreto</a></button>
+      <button className="flex bg-blue-600 py-2 px-3 rounded-lg items-center"><a href="/event/1" >Buscar Amigo Secreto</a></button>
     </div>
   );
 }
